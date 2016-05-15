380d21beed5647e6e3d2626252037ebe9d92b6f7
Matt Carpenter <mattcarpenter@gmail.com>
2016-05-14 17:21:32 -0700

    Babel

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,19 @@ var webpack = require('webpack-stream');
 
 gulp.task('default', function () {
    return gulp.src('src/client/index.js')
-       .pipe(webpack())
+       .pipe(webpack({
+          watch: true,
+           module: {
+               loaders: [{
+                   test: /\.js$/,
+                   exclude: /node_modules/,
+                   loader: 'babel',
+                   query: {
+                       presets: ['es2015']
+                   }
+               }]
+           }
+       }))
        .pipe(rename('bundle.js'))
        .pipe(gulp.dest('public/js'));
 });
